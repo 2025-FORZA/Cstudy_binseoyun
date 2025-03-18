f27a0e80b28d8e76b628ea96b8bd09daf9b7d85d
binseoyun <sunnybin04@gmail.com>
2025-03-18 13:25:08 +0900

    1_Lab01

---
+++ 1_Lab01_1.c
@@ -0,0 +1,7 @@
+// 1_Lab01_1
+//น้มุ 2557น๘ Hello World
+#include<stdio.h>
+int main() {
+	printf("Hello World!");
+	return 0;
+}
---
+++ 1_Lab01_2.c
@@ -0,0 +1,9 @@
+// 1_Lab01_2
+//น้มุ 1000น๘ A+B
+#include <stdio.h>
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	return 0;
+}
